4fc814fd5255c118419ba72664852084d80b8fa3
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-09-03 21:15:28 +0300

    finish

--- use-popcorn/src/StarRating.js
+++ use-popcorn/src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const containerStyle = {
   display: 'flex',
@@ -10,24 +11,50 @@ const starContaierStyle = {
   display: 'flex',
 };
 
-const textStyle = {
-  lineHeight: '1',
-  margin: '0',
+StartRating.propTypes = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  defaultRating: PropTypes.number,
+  onSetRating: PropTypes.func,
+  className: PropTypes.string,
 };
-export default function StartRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+
+export default function StartRating({
+  maxRating = 5,
+  color = '#fcc419',
+  size = 48,
+  defaultRating = 0,
+  className = '',
+  onSetRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
+  const textStyle = {
+    lineHeight: '1',
+    margin: '0',
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
+  function handelRating(i) {
+    setRating(i);
+    onSetRating && onSetRating(i);
+  }
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContaierStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            onRating={() => setRating(i + 1)}
+            onRating={() => handelRating(i + 1)}
             onEnter={() => setTempRating(i + 1)}
             onLeave={() => setTempRating(0)}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            color={color}
+            size={size}
           />
         ))}
       </div>
@@ -36,14 +63,14 @@ export default function StartRating({ maxRating = 5 }) {
   );
 }
 
-const starStyle = {
-  width: '48px',
-  height: '48px',
-  display: 'block',
-  cursor: 'pointer',
-};
+function Star({ onRating, full, onEnter, onLeave, size, color }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: 'block',
+    cursor: 'pointer',
+  };
 
-function Star({ onRating, full, onEnter, onLeave }) {
   return (
     <span
       role='button'
@@ -56,8 +83,8 @@ function Star({ onRating, full, onEnter, onLeave }) {
         <svg
           xmlns='http://www.w3.org/2000/svg'
           viewBox='0 0 20 20'
-          fill='#000'
-          stroke='#000'
+          fill={color}
+          stroke={color}
         >
           <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
         </svg>
@@ -66,7 +93,7 @@ function Star({ onRating, full, onEnter, onLeave }) {
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
           viewBox='0 0 24 24'
-          stroke='#000'
+          stroke={color}
         >
           <path
             strokeLinecap='round'
@@ -79,17 +106,3 @@ function Star({ onRating, full, onEnter, onLeave }) {
     </span>
   );
 }
-
-/*
-FULL STAR
-
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 20 20"
-  fill="#000"
-  stroke="#000"
->
-  <path
-    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-  />
-</svg>*/
--- use-popcorn/src/index.js
+++ use-popcorn/src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
-import StartRating from './StartRating';
+import StarRating from './StarRating';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color='blue' onSetRating={setMovieRating} />
+      <p>this movie was rated {movieRating} times</p>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <StartRating maxRating={5} />
+    <StarRating maxRating={5} defaultRating={3} />
+    <StarRating maxRating={7} />
+    <Test />
   </React.StrictMode>
 );
