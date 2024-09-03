1e9bae5b43f64df38db69e61c192fcbef64b9b6a
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-09-03 18:16:26 +0300

    stars

--- use-popcorn/src/index.js
+++ use-popcorn/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
+// import './index.css';
+// import App from './App';
+import StartRating from './StartRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StartRating maxRating={5} />
   </React.StrictMode>
 );
