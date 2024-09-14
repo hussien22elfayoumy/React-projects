bb686dac1a0044afe0ee48c1a776bf705042dae3
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-09-14 23:34:25 +0300

    quiz

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,9 +1,81 @@
+import { useEffect, useReducer } from 'react';
 import Header from './components/Header';
+import Main from './components/Main';
+import Loader from './components/Loader';
+import Error from './components/Error';
+import StartScreen from './components/StartScreen';
+import Question from './components/Question';
+
+const initialState = {
+  questions: [],
+  // loadin, error, ready , active, finished
+  status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    case 'start':
+      return {
+        ...state,
+        status: 'active',
+      };
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    default:
+      throw new Error('Action is unknown');
+  }
+}
 
 function App() {
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  const questionsNum = questions?.length;
+
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
+
   return (
     <div className='app'>
       <Header />
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'erroe' && <Error />}
+        {status === 'ready' && (
+          <StartScreen questionsNum={questionsNum} dispatch={dispatch} />
+        )}
+        {status === 'active' && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
+      </Main>
     </div>
   );
 }
