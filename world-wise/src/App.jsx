3e93257bc292f391c460e3875733dc847883aa81
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-09-14 21:34:17 +0300

    end

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
@@ -8,6 +8,8 @@ import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
+import City from './components/City';
+import Form from './components/Form';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -41,19 +43,17 @@ function App() {
           <Route path='login' element={<Login />} />
 
           <Route path='app' element={<AppLayout />}>
-            <Route
-              index
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+            <Route index element={<Navigate replace to='cities' />} />
             <Route
               path='cities'
               element={<CityList cities={cities} isLoading={isLoading} />}
             />
+            <Route path='cities/:id' element={<City />} />
             <Route
               path='countries'
               element={<CountryList cities={cities} isLoading={isLoading} />}
             />
-            <Route path='form' element={<p>a form</p>} />
+            <Route path='form' element={<Form />} />
           </Route>
 
           <Route path='*' element={<PageNotFound />} />
---
+++ world-wise/src/components/Button.jsx
@@ -0,0 +1,17 @@
+import styles from './Button.module.css';
+import PropTypes from 'prop-types';
+
+function Button({ children, onClick, type }) {
+  return (
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+      {children}
+    </button>
+  );
+}
+Button.propTypes = {
+  children: PropTypes.any,
+  onClick: PropTypes.any,
+  type: PropTypes.any,
+};
+
+export default Button;
