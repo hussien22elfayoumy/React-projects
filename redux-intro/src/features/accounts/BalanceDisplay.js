2db088d28d6f02c18f7a6e221298df6d55c41d1e
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-09-28 17:52:27 +0300

    connecting react to redux useSelector useDispatch

--- redux-intro/src/features/accounts/BalanceDisplay.js
+++ redux-intro/src/features/accounts/BalanceDisplay.js
@@ -1,12 +1,31 @@
+import { connect, useSelector } from 'react-redux';
+
 function formatCurrency(value) {
-  return new Intl.NumberFormat("en", {
-    style: "currency",
-    currency: "USD",
+  return new Intl.NumberFormat('en', {
+    style: 'currency',
+    currency: 'USD',
   }).format(value);
 }
 
 function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+  const balance = useSelector((store) => store.account.balance);
+
+  return <div className='balance'>{formatCurrency(balance)}</div>;
 }
 
 export default BalanceDisplay;
+
+/* 
+NOTE: the old way to connect components to redux
+
+function BalanceDisplay({ balance }) {
+  // const balance = useSelector((store) => store.account.balance);
+
+  return <div className='balance'>{formatCurrency(balance)}</div>;
+}
+function mapStateProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+export default connect(mapStateProps)(BalanceDisplay); */
