73b04afc03e3993e3fc6ca610e000d1407cc1b47
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-09-29 12:35:18 +0300

    app layout

---
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import CartOverview from '../features/cart/CartOverview';
+import Header from './Header';
+
+function AppLayout() {
+  return (
+    <div>
+      <Header />
+
+      <Outlet />
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
---
+++ fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Link to='/'>Fase React Pizzs Co.</Link>
+
+      <p>Hussien</p>
+    </header>
+  );
+}
+
+export default Header;
