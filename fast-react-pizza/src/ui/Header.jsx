8c2a44a54fc35bf3afce0a642395f1dd34e0b008
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-09-29 13:51:35 +0300

    search orders

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
   return (
     <header>
       <Link to='/'>Fase React Pizzs Co.</Link>
-
+      <SearchOrder />
       <p>Hussien</p>
     </header>
   );
