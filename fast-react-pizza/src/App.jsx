7d7514a87bba55072b63a1e8578aac43d1d40324
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-09-29 12:57:02 +0300

    render as you fetch strategy

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Home from './ui/Home';
-import Menu from './features/menu/Menu';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
@@ -19,6 +19,8 @@ const router = createBrowserRouter([
       {
         path: '/menu',
         element: <Menu />,
+        // render as you fetch strategy
+        loader: menuLoader,
       },
 
       {
