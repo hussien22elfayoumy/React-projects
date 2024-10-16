7d97f81e23463c6a566ff06e86eee0205a9430a9
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-16 21:06:38 +0300

    tailwind

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,14 +1,17 @@
-import { Link } from 'react-router-dom';
-import SearchOrder from '../features/order/SearchOrder';
+import { Link } from 'react-router-dom'
+import SearchOrder from '../features/order/SearchOrder'
+import UserName from '../features/user/UserName'
 
 function Header() {
   return (
-    <header>
-      <Link to='/'>Fase React Pizzs Co.</Link>
+    <header className='border-b border-stone-300 bg-yellow-500 px-4 py-3 uppercase sm:px-6'>
+      <Link to='/' className='tracking-widest'>
+        Fase React Pizzs Co.
+      </Link>
       <SearchOrder />
-      <p>Hussien</p>
+      <UserName />
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
