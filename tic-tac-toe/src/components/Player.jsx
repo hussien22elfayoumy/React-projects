6032ef9ba932d22e79529331e7ceeac864d9bdb7
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-12-15 21:04:24 +0200

    feat: handle players names

---
+++ tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((isEditing) => !isEditing);
+  }
+
+  function handleChange(e) {
+    setPlayerName(e.target.value);
+  }
+
+  let player = <span className='player-name'>{playerName}</span>;
+
+  if (isEditing) {
+    player = (
+      <input required onChange={handleChange} value={playerName} type='text' />
+    );
+  }
+
+  return (
+    <li>
+      <span className='player'>
+        {player}
+        <span className='player-symbol'>{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
+}
