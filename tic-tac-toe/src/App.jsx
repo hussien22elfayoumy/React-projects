59a269f4cc80d0681f4873bd7b30d426e0a6993c
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-12-16 15:19:53 +0200

    feat: handle winning

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -4,6 +4,13 @@ import Player from './components/Player';
 import Log from './components/Log';
 import { WINNING_COMBINATIONS } from './winning-combinations';
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+// TODO: know who is the active player driveen from game turns
 function driveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
 
@@ -16,9 +23,39 @@ function driveActivePlayer(gameTurns) {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  let activePlayer = driveActivePlayer(gameTurns);
 
-  function handleSelectedPlayer(rowIndex, colIndex) {
+  let activePlayer = driveActivePlayer(gameTurns);
+  let gameBoard = initialGameBoard;
+  let winner;
+
+  // TODO: display user symbol on the game board
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  // TODO: chek if there is wining combination
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+  console.log(winner);
+
+  // TODO: handel turns whenever clicks on board
+  function handleSelectedSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
       let currentPlayer = driveActivePlayer(prevTurns);
 
@@ -47,7 +84,7 @@ function App() {
           />
         </ol>
 
-        <GameBoard onSelectSquare={handleSelectedPlayer} turns={gameTurns} />
+        <GameBoard onSelectSquare={handleSelectedSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
