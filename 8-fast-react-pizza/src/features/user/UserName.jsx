d18722dcb18e586b7da4afe2abc73172fc51b2cc
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-01-11 17:29:47 +0200

    feat: add redux to the app create user

--- 8-fast-react-pizza/src/features/user/UserName.jsx
+++ 8-fast-react-pizza/src/features/user/UserName.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from 'react-redux';
+
 function UserName() {
-  return <div className='hidden text-sm font-semibold md:block'>Hussien</div>
+  const userName = useSelector((state) => state.user.userName);
+
+  if (!userName) return null;
+
+  return (
+    <div className='hidden text-sm font-semibold md:block'>{userName}</div>
+  );
 }
 
-export default UserName
+export default UserName;
